7631950f3689f175b537c70136e5deae3f808a82
Daeseong Kim <16497919+dkimds@users.noreply.github.com>
2025-04-01 23:10:07 +0900

    Create evaluation_assistent.py
    [Feature] Streamlit front-end

---
+++ evaluation_assistant.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from modules.streamlit_app import main
+
+st.title("Patent Evaluation Assistant")
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+    # Upload patent
+    bytes_data = uploaded_file.getvalue()
+    file_path = "./temp.pdf"
+    with open(file_path, "wb") as f:
+        f.write(bytes_data)
+
+with st.form("my_form"):
+    text = st.text_area(
+        "Enter topic:",
+        "The benefits of adopting the following patent as a business model",)
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        main(text)
